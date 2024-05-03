0902f655a9641cf5fb803e66103bd5532293e1f5
Daniel Karanja <sirkaranja12@gmail.com>
2024-05-03 14:32:27 +0300

    sms intergration with africastalking

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Appointment;
+use App\Traits\SMSNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AppointmentController extends Controller
 {
+    use SMSNotification;
+
     public function store(Request $request)
     {
         // Validate the incoming request data
@@ -22,8 +26,8 @@ class AppointmentController extends Controller
         ]);
 
         // Determine the default status based on the user's role
-        $defaultStatus = Auth::user()->role === 'secretary' || Auth::user()->role === 'manager' 
-            ? 'pending approval' 
+        $defaultStatus = Auth::user()->role === 'secretary' || Auth::user()->role === 'manager'
+            ? 'pending approval'
             : 'approved';
 
         // Add the default status to the validated data
@@ -41,6 +45,12 @@ class AppointmentController extends Controller
         $appointment = Appointment::findOrFail($id);
         $appointment->delete();
         return response()->json(['message' => 'Appointment deleted successfully'], 200);
+    }
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect()->route('login');
     }
 
     public function edit($id)
@@ -63,76 +73,129 @@ class AppointmentController extends Controller
             'description' => 'nullable|string',
             'status' => 'required|string|max:255',
         ]);
-    
+
         // Find the appointment by ID
         $appointment = Appointment::findOrFail($id);
-    
+
         // Update the appointment with the validated data
         $appointment->update($validatedData);
-    
+
         // Return a success response
         return response()->json(['message' => 'Appointment updated successfully']);
     }
 
+    
 
-    public function reschedule(Request $request, $id)
+    // public function reschedule(Request $request, $id)
+    // {
+    //     // Validate the request data for rescheduling
+    //     $validatedData = $request->validate([
+    //         'name' => 'required|string|max:255',
+    //         'category' => 'required|string|max:255',
+    //         'phone' => 'required|string|max:255',
+    //         'appointment_date' => 'required|date',
+    //         'appointment_time' => 'required|string|max:255',
+    //         'description' => 'nullable|string',
+    //     ]);
+
+    //     // Find the appointment by ID
+    //     $appointment = Appointment::findOrFail($id);
+
+    //     // Update the appointment with the validated data
+    //     $appointment->update($validatedData);
+
+    //     // Return a success response
+    //     return response()->json(['message' => 'Appointment rescheduled successfully']);
+    // }
+
+
+
+
+    public function changeStatus($id, $status)
     {
-        // Validate the request data for rescheduling
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'category' => 'required|string|max:255',
-            'phone' => 'required|string|max:255',
-            'appointment_date' => 'required|date',
-            'appointment_time' => 'required|string|max:255',
-            'description' => 'nullable|string',
-        ]);
-    
-        // Find the appointment by ID
-        $appointment = Appointment::findOrFail($id);
-    
-        // Update the appointment with the validated data
-        $appointment->update($validatedData);
-    
-        // Return a success response
-        return response()->json(['message' => 'Appointment rescheduled successfully']);
+        try {
+            DB::beginTransaction();
+            // Find the appointment by ID
+            $appointment = Appointment::findOrFail($id);
+
+            if ($status == 'approved') {
+                // Update the status to 'approved'
+                $appointment->update(['status' => 'approved']);
+
+                // send sms notification
+                $message = 'Hello ' . $appointment->name . ' your appointment is scheduled on ' . $appointment->appointment_date . ' at ' . $appointment->appointment_time;
+            } else if ($status == 'rejected') {
+                // Update the status to 'rejected'
+                $appointment->update(['status' => 'rejected']);
+
+                // send sms notification
+                $message = 'Hello ' . $appointment->name . ', we are sorry your appointment did not go through at this time. Kindly try rescheduling at another time.';
+            } else {
+                return response()->json(['error' => 'status does not exist']);
+            }
+
+            $this->sendSms($appointment->phone, $message);
+            // $phone = ['+254798343427'];
+            // $this->sendSms($phone, $message);
+
+            // Return a success response
+            DB::commit();
+            return response()->json(['message' => 'Appointment approved successfully']);
+        } catch (\Throwable $th) {
+            DB::rollback();
+            dd($th->getMessage());
+            return response()->json(['error' => $th->getMessage()]);
+        }
     }
-    
-
-    
-
-
 
 
 
 
     public function approve($id)
-{
-    // Find the appointment by ID
-    $appointment = Appointment::findOrFail($id);
+    {
+        try {
+            DB::beginTransaction();
+            // Find the appointment by ID
+            $appointment = Appointment::findOrFail($id);
 
-    // Update the status to 'approved'
-    $appointment->update(['status' => 'approved']);
+            // Update the status to 'approved'
+            $appointment->update(['status' => 'approved']);
 
-    // Return a success response
-    return response()->json(['message' => 'Appointment approved successfully']);
+            // send sms notification
+            $message = 'Hello ' . $appointment->name . ' your appointment is scheduled on ' . $appointment->appointment_date . ' at ' . $appointment->appointment_time;
+            // $this->sendSms($appointment->phone, $message);
+            $phone = ['+25474142479', '+254798343427'];
+            $this->sendSms($phone, $message);
+
+            // Return a success response
+            DB::commit();
+            return response()->json(['message' => 'Appointment approved successfully']);
+        } catch (\Throwable $th) {
+            DB::rollback();
+            dd($th->getMessage());
+            return response()->json(['error' => $th->getMessage()]);
+        }
+    }
+
+    public function reject($id)
+    {
+        // Find the appointment by ID
+        $appointment = Appointment::findOrFail($id);
+
+        // Update the status to 'rejected'
+        $appointment->update(['status' => 'rejected']);
+
+        // Return a success response
+        return response()->json(['message' => 'Appointment rejected successfully']);
+    }
+    public function show($id)
+    {
+        $appointment = Appointment::findOrFail($id);
+        return response()->json($appointment);
+    }
 }
 
-public function reject($id)
-{
-    // Find the appointment by ID
-    $appointment = Appointment::findOrFail($id);
-
-    // Update the status to 'rejected'
-    $appointment->update(['status' => 'rejected']);
-
-    // Return a success response
-    return response()->json(['message' => 'Appointment rejected successfully']);
-}
-public function show($id)
-{
-    $appointment = Appointment::findOrFail($id);
-    return response()->json($appointment);
-}
 
 
-}
+
+
